da4c8d58820f654777233838b90873b7fec5397e
João Salomão <joaosalomao.613@gmail.com>
2020-08-29 01:25:17 -0300

    Ajustando sql builder

--- sql_generator.py
+++ sql_generator.py
@@ -1,3 +1,5 @@
+import numpy
+import pandas as pd
 
 ALLOWED_EXTENSIONS = {'xlsx', 'csv'}
 
@@ -9,10 +11,10 @@ def file_is_allowed(filename):
 def create_insert_sql(df, table_name):
     sql = 'INSERT INTO ' + table_name + '('
     columns = df.columns
-    lenColumns = len(columns)
+    len_columns = len(columns)
     
-    for i in range(lenColumns):
-        if i < lenColumns - 1:
+    for i in range(len_columns):
+        if i < len_columns - 1:
             sql = sql + columns[i] + ','
         else:
             sql = sql + columns[i] + ') VALUES '
@@ -20,16 +22,16 @@ def create_insert_sql(df, table_name):
     temp = ''
     formatter = ''
     for i in range(df.shape[0]):
-        for k in range(lenColumns):
-            value = str(df[columns[k]][i])
+        for k in range(len_columns):
+            value = parse_value_to_sql_builder(df[columns[k]][i])
             if k == 0:
-                formatter = "('{}',"
+                formatter = "({},"
                 temp = formatter.format(value)
-            elif k < lenColumns-1:
-                formatter = temp + "'{}',"
+            elif k < len_columns-1:
+                formatter = temp + "{},"
                 temp = formatter.format(value)
             else:
-                formatter = temp + "'{}')"
+                formatter = temp + "{})"
                 temp = formatter.format(value)
     
         if i < df.shape[0] - 1 :
@@ -41,22 +43,31 @@ def create_insert_sql(df, table_name):
 def create_update_sql(df, table_name):
     sql = ''
     columns = df.columns
-    lenColumns = len(columns)
+    len_columns = len(columns)
     base = 'UPDATE ' + table_name + ' SET '
 
     formatter = ''
     temp = base
     for i in range(df.shape[0]):
-        for k in range(lenColumns):
+        for k in range(len_columns):
             column = columns[k]
-            value = str(df[columns[k]][i])
-
-            if k < lenColumns - 1:
-                formatter = temp + " " + column + " = '{}',"
+            value = parse_value_to_sql_builder(df[columns[k]][i])
+            if k < len_columns - 2:
+                formatter = temp + " " + column + " = {},"
+                temp = formatter.format(value)
+            elif k < len_columns - 1:
+                formatter = temp + " " + column + " = {}"
                 temp = formatter.format(value)
             else:
                 formatter = temp + " " + column + " {}; "
-                temp = formatter.format(value)
+                temp = formatter.format(str(df[columns[k]][i]))
         sql = sql + temp
         temp = base
     return sql
+
+
+def parse_value_to_sql_builder(value):
+    if isinstance(value, str) or isinstance(value, pd._libs.tslibs.timestamps.Timestamp):
+        return "'{}'".format(value)
+    if isinstance(value, numpy.int64) or isinstance(value, numpy.float64):
+        return "{}".format(str(value))
