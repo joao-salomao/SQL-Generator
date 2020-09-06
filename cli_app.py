28ff3a76c95f4e4610198c01d31078e3967b9c35
João Salomão <joaosalomao.613@gmail.com>
2020-09-06 00:50:28 -0300

    Refatorando módulo para funcionar com a nova estrutura do sql_generator

--- cli_app.py
+++ cli_app.py
@@ -1,45 +1,39 @@
 import sys
-from sql_generator import ALLOWED_OPERATIONS, create_insert_sql, create_update_sql
-try:
-    import pandas as pd
-except ImportError:
-    print("Can't find pandas module")
-    print("Type the command 'pip install pandas' to install the module")
+import sql_generator as g
 
 
 ## Install to work with Excel ##
 # pip install xlrd
 
 
-##  Enable to fix encoding errors ##
-#reload(sys)
-#systemEncoder = sys.getfilesystemencoding()
-#sys.setdefaultencoding(systemEncoder)
+reload(sys)
+systemEncoder = sys.getfilesystemencoding()
+sys.setdefaultencoding(systemEncoder)
 
-def main():
+def validate_args():
     if len(sys.argv) < 4:
-        print('To few arguments')
+        print('> To few arguments <')
         print('You must pass the operation, the table name and the name of the file to be read')
         print('Example: python cli_app.py insert users users.xlsx')
+        return False
+
+    if g.operation_is_allowed(sys.argv[1]) == False:
+        print('> Operation not allowed <')
+        print('The allowed operations are: insert, update, delete.')
+        return False
+
+    if g.file_is_allowed(sys.argv[3]) == False:
+        print('> File not allowed <')
+        print('The allowed extensions are: xlsx and csv.')
+        return False
+
+def main():
+    if validate_args() == False:
         return
-
-    if sys.argv[1] not in ALLOWED_OPERATIONS:
-        print('Operation type invalid')
-        return
-
-    fileExtension = sys.argv[3].split('.')[1]
-    if fileExtension == 'xlsx':
-        df = pd.read_excel(sys.argv[3], sheet_name='Sheet1')
-    else:
-        df = pd.read_csv(sys.argv[3])
-
-
-    sql = ''
-    tableName = sys.argv[2]
-    if sys.argv[1] == 'insert':
-        sql = create_insert_sql(df, tableName)
-    else:
-        sql = create_update_sql(df, tableName)
+    operation = sys.argv[1]
+    table_name = sys.argv[2]
+    file = sys.argv[3]
+    sql = g.generate_sql(file, table_name, operation)
 
     open("generated_sql.sql", "w").write(sql)
     print('SQL successfully generated !')
