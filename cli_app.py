35e9d5ba54d3b686cafa0c44ba2cac78a42d8514
João Salomão <joaosalomao.613@gmail.com>
2020-09-06 01:16:44 -0300

    Importando apenas as propriedades e funções necessárias e removendo a setagem do system encoder

--- cli_app.py
+++ cli_app.py
@@ -1,28 +1,19 @@
-import sys
-import sql_generator as g
-
-
-## Install to work with Excel ##
-# pip install xlrd
-
-
-reload(sys)
-systemEncoder = sys.getfilesystemencoding()
-sys.setdefaultencoding(systemEncoder)
+from sys import argv
+from sql_generator import generate_sql, operation_is_allowed, file_is_allowed
 
 def validate_args():
-    if len(sys.argv) < 4:
+    if len(argv) < 4:
         print('> To few arguments <')
         print('You must pass the operation, the table name and the name of the file to be read')
         print('Example: python cli_app.py insert users users.xlsx')
         return False
 
-    if g.operation_is_allowed(sys.argv[1]) == False:
+    if operation_is_allowed(argv[1]) == False:
         print('> Operation not allowed <')
         print('The allowed operations are: insert, update, delete.')
         return False
 
-    if g.file_is_allowed(sys.argv[3]) == False:
+    if file_is_allowed(argv[3]) == False:
         print('> File not allowed <')
         print('The allowed extensions are: xlsx and csv.')
         return False
@@ -30,10 +21,10 @@ def validate_args():
 def main():
     if validate_args() == False:
         return
-    operation = sys.argv[1]
-    table_name = sys.argv[2]
-    file = sys.argv[3]
-    sql = g.generate_sql(file, table_name, operation)
+    operation = argv[1]
+    table_name = argv[2]
+    file = argv[3]
+    sql = generate_sql(file, table_name, operation)
 
     open("generated_sql.sql", "w").write(sql)
     print('SQL successfully generated !')
