a967a2898f1b9f7f5444ee96d326b9ab83231a84
João Salomão <joaosalomao.613@gmail.com>
2020-09-09 01:22:15 -0300

    Testando método create_insert_sql

--- tests.py
+++ tests.py
@@ -1,7 +1,8 @@
-import unittest
-from sql_generator import operation_is_allowed, file_is_allowed
+from pandas import DataFrame
+from unittest import TestCase, main
+from sql_generator import operation_is_allowed, file_is_allowed, create_insert_sql
 
-class TestValidations(unittest.TestCase):
+class TestValidations(TestCase):
     def test_should_allow_operation(self):
         self.assertTrue(operation_is_allowed('insert'))
         self.assertTrue(operation_is_allowed('update'))
@@ -24,5 +25,35 @@ class TestValidations(unittest.TestCase):
         self.assertFalse(file_is_allowed('users.ots'))
 
 
+class TestCreateInsertSQL(TestCase):
+    def setUp(self):
+        self.data = [
+            {
+                "table_name": "users",
+                "dataframe": DataFrame({
+                    'id': [1,2,3],
+                    'name': ['Jane', 'Victor', 'Bob'],
+                    'salary': [2500.6, 1000.5, 3450.1]
+                }),
+                "expected": "INSERT INTO users(id, name, salary) VALUES (1, 'Jane', 2500.6), (2, 'Victor', 1000.5), (3, 'Bob', 3450.1);"
+            },
+            {
+                "table_name": "companies",
+                "dataframe": DataFrame({
+                    'id': [333,414,578, 1234],
+                    'company_name': ['Apple', 'AOC', 'Samsung', 'LG'],
+                    'market_price': [300000000000, 99999999, 46464646464646, 123123123123]
+                }),
+                "expected": "INSERT INTO companies(id, company_name, market_price) VALUES (333, 'Apple', 300000000000), (414, 'AOC', 99999999), (578, 'Samsung', 46464646464646), (1234, 'LG', 123123123123);"
+            }
+        ]
+
+    def test_should_create_insert_sql(self):
+        for case in self.data:
+            df =case["dataframe"]
+            expected = case["expected"]
+            table_name = case["table_name"]
+            self.assertEqual(expected, create_insert_sql(df, table_name))
+
 if __name__ == '__main__':
-    unittest.main()
+    main(verbosity=2)
