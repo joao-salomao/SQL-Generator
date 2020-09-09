c0ed201b3c065a7e6f7ee471a42edf124f849d51
João Salomão <joaosalomao.613@gmail.com>
2020-09-09 02:53:21 -0300

    Definindo uma classe de teste por módulo

--- tests.py
+++ tests.py
@@ -1,33 +1,10 @@
 from pandas import DataFrame
 from unittest import TestCase, main
-from sql_generator import operation_is_allowed, file_is_allowed, create_insert_sql, create_update_sql, create_delete_sql
+from sql_generator import operation_is_allowed, file_is_allowed, create_insert_sql, create_update_sql, create_delete_sql, generate_sql
 
-class TestValidations(TestCase):
-    def test_should_allow_operation(self):
-        self.assertTrue(operation_is_allowed('insert'))
-        self.assertTrue(operation_is_allowed('update'))
-        self.assertTrue(operation_is_allowed('delete'))
-
-    def test_should_not_allow_operation(self):
-        self.assertFalse(operation_is_allowed('create'))
-        self.assertFalse(operation_is_allowed('remove'))
-        self.assertFalse(operation_is_allowed('use'))
-
-    def test_should_allow_file(self):
-        self.assertTrue(file_is_allowed('users.xlsx'))
-        self.assertTrue(file_is_allowed('users.csv'))
-
-    def test_should_not_allow_file(self):
-        self.assertFalse(file_is_allowed('users.xls'))
-        self.assertFalse(file_is_allowed('users.ods'))
-        self.assertFalse(file_is_allowed('users.exe'))
-        self.assertFalse(file_is_allowed('users.py'))
-        self.assertFalse(file_is_allowed('users.ots'))
-
-
-class TestCreateInsertSQL(TestCase):
+class TestSQLGenerator(TestCase):
     def setUp(self):
-        self.data = [
+        self.data_insert = [
             {
                 "table_name": "users",
                 "dataframe": DataFrame({
@@ -48,20 +25,7 @@ class TestCreateInsertSQL(TestCase):
             }
         ]
 
-    def test_should_create_insert_sql(self):
-        for case in self.data:
-            df =case["dataframe"]
-            expected = case["expected"]
-            table_name = case["table_name"]
-            result = create_insert_sql(df, table_name)
-
-            self.assertIsInstance(result, str)
-            self.assertEqual(expected, result)
-            
-
-class TestCreateUpdateSQL(TestCase):
-    def setUp(self):
-        self.data = [
+        self.data_update = [
             {
                 "table_name": "accounts",
                 "dataframe": DataFrame({
@@ -81,21 +45,7 @@ class TestCreateUpdateSQL(TestCase):
             },
         ]
 
-    def test_should_create_update_sql(self):
-        for case in self.data:
-            df =case["dataframe"]
-            expected = case["expected"]
-            table_name = case["table_name"]
-            result = create_update_sql(df, table_name)
-
-            self.assertIsInstance(result, str)
-            self.assertEqual(expected, result)
-
-
-
-class TestCreateDeleteSQL(TestCase):
-    def setUp(self):
-        self.data = [
+        self.data_delete = [
             {
                 "table_name": "accounts",
                 "dataframe": DataFrame({
@@ -114,8 +64,29 @@ class TestCreateDeleteSQL(TestCase):
             },
         ]
 
+    def test_should_create_insert_sql(self):
+        for case in self.data_insert:
+            df =case["dataframe"]
+            expected = case["expected"]
+            table_name = case["table_name"]
+            result = create_insert_sql(df, table_name)
+
+            self.assertIsInstance(result, str)
+            self.assertEqual(expected, result)
+
+    def test_should_create_update_sql(self):
+        for case in self.data_update:
+            df =case["dataframe"]
+            expected = case["expected"]
+            table_name = case["table_name"]
+            result = create_update_sql(df, table_name)
+
+            self.assertIsInstance(result, str)
+            self.assertEqual(expected, result)
+
+
     def test_should_create_delete_sql(self):
-        for case in self.data:
+        for case in self.data_delete:
             df =case["dataframe"]
             expected = case["expected"]
             table_name = case["table_name"]
@@ -124,6 +95,41 @@ class TestCreateDeleteSQL(TestCase):
             self.assertIsInstance(result, str)
             self.assertEqual(expected, result)
 
+    def test_should_generate_sql(self):
+        result_insert = generate_sql("test_insert.xlsx", 'users', 'insert')
+        result_update = generate_sql("test_update.xlsx", 'users', 'update')
+        result_delete = generate_sql("test_delete.xlsx", 'users', 'delete')
+
+        self.assertIsInstance(result_insert, str)
+        self.assertTrue(len(result_insert) > 0)
+
+        self.assertIsInstance(result_update, str)
+        self.assertTrue(len(result_update) > 0)
+
+        self.assertIsInstance(result_delete, str)
+        self.assertTrue(len(result_delete) > 0)
+
+
+    def test_should_allow_operation(self):
+        self.assertTrue(operation_is_allowed('insert'))
+        self.assertTrue(operation_is_allowed('update'))
+        self.assertTrue(operation_is_allowed('delete'))
+
+    def test_should_not_allow_operation(self):
+        self.assertFalse(operation_is_allowed('create'))
+        self.assertFalse(operation_is_allowed('remove'))
+        self.assertFalse(operation_is_allowed('use'))
+
+    def test_should_allow_file(self):
+        self.assertTrue(file_is_allowed('users.xlsx'))
+        self.assertTrue(file_is_allowed('users.csv'))
+
+    def test_should_not_allow_file(self):
+        self.assertFalse(file_is_allowed('users.xls'))
+        self.assertFalse(file_is_allowed('users.ods'))
+        self.assertFalse(file_is_allowed('users.exe'))
+        self.assertFalse(file_is_allowed('users.py'))
+        self.assertFalse(file_is_allowed('users.ots'))
 
 if __name__ == '__main__':
     main(verbosity=2)
