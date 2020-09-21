63fcf55b1d2a79ba20a35e18c437d1c4a6e72c66
João Salomão <joaosalomao.613@gmail.com>
2020-09-20 23:36:31 -0300

    Tipando todas as função do módulo sql_generator

--- sql_generator/sql_generator.py
+++ sql_generator/sql_generator.py
@@ -1,22 +1,26 @@
-import numpy
-import pandas as pd
+from typing import Union
+from numpy import int64, float64
+from werkzeug.datastructures import FileStorage
+from pandas import read_excel, read_csv, DataFrame
+from pandas._libs.tslibs.timestamps import Timestamp
 
 ALLOWED_EXTENSIONS = {'xlsx', 'csv'}
 ALLOWED_OPERATIONS = {'insert', 'update', 'delete'}
 
-def file_is_allowed(filename):
+def file_is_allowed(filename: str) -> bool:
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def operation_is_allowed(operation):
+def operation_is_allowed(operation: str) -> bool:
     return operation in ALLOWED_OPERATIONS
     
-def parse_value_to_sql_builder(value):
-    if isinstance(value, str) or isinstance(value, pd._libs.tslibs.timestamps.Timestamp):
+def parse_value_to_sql_builder(value: Union[str, int64, float64, Timestamp]) -> str:
+    if isinstance(value, str) or isinstance(value, Timestamp):
         return "'{}'".format(value)
-    if isinstance(value, numpy.int64) or isinstance(value, numpy.float64):
+    if isinstance(value, int64) or isinstance(value, float64):
         return "{}".format(str(value))
+    raise Exception("Error parsing value to sql builder")
 
-def create_insert_sql(df, table_name):
+def create_insert_sql(df: DataFrame, table_name: str) -> str:
     sql = 'INSERT INTO ' + table_name + '('
     columns = df.columns
     len_columns = len(columns)
@@ -48,7 +52,7 @@ def create_insert_sql(df, table_name):
             sql = sql + temp + ';'
     return sql
 
-def create_update_sql(df, table_name):
+def create_update_sql(df: DataFrame, table_name: str) -> str:
     sql = ''
     columns = df.columns
     len_columns = len(columns)
@@ -74,7 +78,7 @@ def create_update_sql(df, table_name):
     return sql.strip()
 
 
-def create_delete_sql(df, table_name):
+def create_delete_sql(df: DataFrame, table_name: str) -> str:
     sql = ''
     columns = df.columns
     len_columns = len(columns)
@@ -97,19 +101,19 @@ def create_delete_sql(df, table_name):
 
     return sql.strip()
 
-def get_dataframe(file, filename):
+def get_dataframe(file: Union[FileStorage, str], filename: str) -> DataFrame:
     if file_is_allowed(filename) == False:
         raise Exception("File not allowed")
 
     try:
         ext = filename.rsplit('.', 1)[1]
         if ext == 'xlsx':
-            return pd.read_excel(file, sheet_name='Sheet1')
-        return pd.read_csv(file)
+            return read_excel(file, sheet_name='Sheet1')
+        return read_csv(file)
     except Exception as e:
         raise e
 
-def generate_sql(file, filename, table_name, operation):
+def generate_sql(file: Union[FileStorage, str], filename: str, table_name: str, operation: str) -> str:
     if operation_is_allowed(operation) == False:
         raise Exception('Operation not allowed')
     
